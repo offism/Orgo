628be1fc9a312e68b3d4e4857b61ed3cc824c5fa
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-27 15:11:23 +0300

    Update mongo.js

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
 async function client () {
-    return await mongoose.connect('mongodb+srv://offism:sma01022002@cluster0.hnbti.mongodb.net/orgo?authSource=admin&replicaSet=atlas-nk6xyl-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
+    return await mongoose.connect("mongodb://127.0.0.1/orgo-mall",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 }
 
 module.exports = {client}
