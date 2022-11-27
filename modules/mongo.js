abd1425145f257257b0e499d9a49fd2a5f58a95d
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-27 15:15:24 +0300

    Update mongo.js

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-async function client () {
-    return await mongoose.connect("mongodb://127.0.0.1/orgo-mall",
+async function client() {
+  return await mongoose.connect(
+    "mongodb://127.0.0.1/orgo-mall",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+    }
+  );
 }
 
-module.exports = {client}
+module.exports = { client };
+
